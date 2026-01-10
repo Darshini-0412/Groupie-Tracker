1594963654d44c477e8b678618a0e1b1c4c6fd5d
ananya <ananyadarshini.pajany@ynov.com>
2026-01-10 22:00:17 +0100

    test artist

--- models/test_artist.go
+++ models/test_artist.go
@@ -1,26 +1,30 @@
 package models
+
 import (
 	"encoding/json"
 	"testing"
 )
-func TestArtistParsing(t *testing.T) {
-    jsonData := `{
 
+func TestArtistParsing(t *testing.T) {
+	jsonData := `{
 		"id": 1,
 		"name": "Queen",
 		"image": "https://example.com/queen.jpg",
 		"members": ["Freddie Mercury", "Brian May"],
 		"creationDate": 1970,
 		"firstAlbum": "1973-07-13",
-		"locations": "Lhttps://api/locations/1",
+		"locations": "https://api/locations/1",
 		"concertDates": "https://api/dates/1",
-		"relations": "https://api/dates/1"
+		"relations": "https://api/relations/1"
 	}`
+
 	var a Artist
 	err := json.Unmarshal([]byte(jsonData), &a)
 	if err != nil {
 		t.Errorf("Failed to parse JSON: %v", err)
 	}
+
 	if a.Name != "Queen" {
 		t.Errorf("Expected name 'Queen', got '%s'", a.Name)
 	}
+}
