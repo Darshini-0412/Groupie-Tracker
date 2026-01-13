4f8769061802673d047220d81647a2cc4b2fdf74
ananya <ananyadarshini.pajany@ynov.com>
2026-01-13 15:09:22 +0100

    test map

--- localisation/map_service.go
+++ localisation/map_service.go
@@ -1,1 +1,75 @@
 package localisation
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
+
+type Location struct {
+	City    string
+	Country string
+	Lat     float64
+	Lon     float64
+}
+
+type nominatimResponse struct {
+	DisplayName string  `json:"display_name"`
+	Lat         float64 `json:"lat,string"`
+	Lon         float64 `json:"lon,string"`
+}
+
+// Cache en mémoire
+var cache = make(map[string]Location)
+
+func SearchLocation(query string) (Location, error) {
+
+	// Vérifier le cache
+	if loc, ok := cache[query]; ok {
+		return loc, nil
+	}
+
+	baseURL := "https://nominatim.openstreetmap.org/search"
+	params := url.Values{}
+	params.Set("q", query)
+	params.Set("format", "json")
+	params.Set("limit", "1")
+
+	// Respect du rate limit
+	time.Sleep(1 * time.Second)
+
+	resp, err := http.Get(baseURL + "?" + params.Encode())
+	if err != nil {
+		return Location{}, err
+	}
+	defer resp.Body.Close()
+
+	var results []nominatimResponse
+	if err := json.NewDecoder(resp.Body).Decode(&results); err != nil {
+		return Location{}, err
+	}
+
+	if len(results) == 0 {
+		return Location{}, fmt.Errorf("no results found for query: %s", query)
+	}
+
+	// Parsing ville-pays
+	parts := strings.Split(results[0].DisplayName, ",")
+	city := strings.TrimSpace(parts[0])
+	country := strings.TrimSpace(parts[len(parts)-1])
+
+	loc := Location{
+		City:    city,
+		Country: country,
+		Lat:     results[0].Lat,
+		Lon:     results[0].Lon,
+	}
+
+	// Stocker dans le cache
+	cache[query] = loc
+
+	return loc, nil
+}
--- ui/ui.go
+++ ui/ui.go
@@ -2,7 +2,7 @@ package ui
 
 import (
 	"fmt"
-	"groupie-tracker/models" // Assurez-vous que le package models définit le type Artist
+	"groupie-tracker/models"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/widget"
@@ -17,7 +17,7 @@ func ShowArtistList(w fyne.Window, artists []models.Artist) {
 			return widget.NewLabel("template")
 		},
 		func(i widget.ListItemID, o fyne.CanvasObject) {
-			o.(*widget.Label).SetText(fmt.Sprintf("%s - %s", artists[i].Name, artists[i].Image)) // Ajustez selon les champs de Artist
+			o.(*widget.Label).SetText(fmt.Sprintf("%s - %s", artists[i].Name, artists[i].Image))
 		},
 	)
 	w.SetContent(list)
